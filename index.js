6c89e052877ba2dcf90a6153219fc4316f25fa39
Antonio MalvaGomes <antonio@distru.com>
2022-03-29 11:32:19 -0500

    Prevent invalid await

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const core = require("@actions/core");
-const {context, GitHub} = require("@actions/github");
-const _ = require("lodash"); 
+const { context, GitHub } = require("@actions/github");
+const _ = require("lodash");
 
 try {
-  const client = new GitHub(core.getInput('token', {required: true}))
+  const client = new GitHub(core.getInput("token", { required: true }));
 
-  const base = context.payload.before
-  const head = context.payload.after
+  const base = context.payload.before;
+  const head = context.payload.after;
 
   const compareResponse = await client.repos.compareCommits({
     base,
     head,
     owner: context.repo.owner,
-    repo: context.repo.repo
+    repo: context.repo.repo,
   });
 
   if (compareResponse.status !== 200) {
@@ -24,28 +24,38 @@ try {
 
   const commits = response.data.commits;
 
-  const pullRequests = commits.map((commit) =>
-    (await client.repos.listPullRequestsAssociatedWithCommit(
-{owner: context.repo.owner,
-repo: context.repo.repo,
-commit: commit.tree.sha}
-    ).data));
+  const pullRequests = await Promise.all(
+    commits.map(async (commit) => {
+      return await client.repos.listPullRequestsAssociatedWithCommit({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        commit: commit.tree.sha,
+      }).data;
+    })
+  );
 
   const uniquePullRequests = _.uniqBy(pullRequests, (pr) => pr.number);
 
-  const informationToReport = uniquePullRequests.filter((pr) =>
-    pr.labels.find((label) => label.name === core.getInput('label', {required: true}))).map((pr) => {
+  const informationToReport = uniquePullRequests
+    .filter((pr) =>
+      pr.labels.find(
+        (label) => label.name === core.getInput("label", { required: true })
+      )
+    )
+    .map((pr) => {
       // [some text](https://www.loom.com/share/2fb40c442cf8437c8a5bfd43e9a2e4b4)
       // remove first match because we don't want the whole PR body, only the links
-      const loomLinks = pr.body.match(/\[.*\]\((https:\/\/www.loom.com.*)\)/).slice(1, -1);
+      const loomLinks = pr.body
+        .match(/\[.*\]\((https:\/\/www.loom.com.*)\)/)
+        .slice(1, -1);
 
       return {
         authorLogin: pr.user.login,
         loomLinks,
         prLink: pr.url,
-        prTitle: pr.title
+        prTitle: pr.title,
       };
-    })
+    });
 
   core.setOutput("pull-request-information", informationToReport);
 } catch (error) {
